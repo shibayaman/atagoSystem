86eff9beadb70b6213835232c91b28ada61b5cfc
大﨑 優人 <2180471@MAETEL.ECC.AC.JP>
2019-12-05 16:47:16 +0900

    商品管理を作っていく

---
+++ www/atagoSystem/database/migrations/2019_12_05_064009_create_zaikos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateZaikosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zaikos', function (Blueprint $table) {
+            $table->increments('ITEM_NUMBER',10)--unique();
+            $table->integer('STOCK',3);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zaikos');
+    }
+}
